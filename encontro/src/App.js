95320bd4ea8299770e5527028af12afc08090725
Heliatã Humberto <heliatan3@gmail.com>
2024-06-01 11:37:12 -0300

    ypdate1

--- encontro/src/App.js
+++ encontro/src/App.js
@@ -2,14 +2,14 @@
 import './App.css';
 
 import Input from './components/form/Input';
-
+import ContainerForm from './components/container/ContainerForm'
 
 function App() {
 
   return (
-
-    <Input />
-    
+    <>
+    <ContainerForm />
+    </>
   );
 }
 
