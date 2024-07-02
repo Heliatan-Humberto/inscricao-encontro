5db8391c136e5b842920526341097f14592b1df7
Heliatã Humberto <heliatan3@gmail.com>
2024-07-02 07:13:41 -0300

    conect

--- api/db.js
+++ api/db.js
@@ -3,6 +3,6 @@ import mysql from "mysql"
 export const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "9236",
-    database: "dados"
+    password: "rajxgfKIRAcNxQHGnYyRZFynIqnyMWiz",
+    database: "inscricoes"
 });
